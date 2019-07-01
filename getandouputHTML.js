142fdb3c6ea19c8f99c1b138d238f3df21f139f1
idetips <50294999+idetips@users.noreply.github.com>
2019-07-02 02:53:16 +0700

    Create getandouputHTML.js

---
+++ getandouputHTML.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<div id="myList">
+  <a href="https://kangracik.blogspot.com/">Coffee</a>
+  <a href="https://kangracik.blogspot.com/">Tea</a>
+</div>
+
+
+<p id="demo"></p>
+
+<script>
+
+  var x = document.getElementById("myList").innerHTML;
+  document.getElementById("demo").innerHTML = x;
+
+</script>
+
+</body>
+</html>
